94f358c63a6a9a9ef15f2f29dedfdee99d4c478d
XanderB88 <alexxanderbykov@gmail.com>
2021-09-01 03:35:51 +0400

    Add README and .gitignore files

---
+++ Weather Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather Application
+//
+//  Created by Alexander on 01.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
