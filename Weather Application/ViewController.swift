672f0abc00de4634c1ce80f03e80f321ee0dc438
XanderB88 <alexxanderbykov@gmail.com>
2021-09-04 05:21:48 +0400

    Update weather card. Add humidity and wind elements.

--- Weather Application/ViewController.swift
+++ Weather Application/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - Background of screen
     lazy var backgroundImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = #imageLiteral(resourceName: "white-clouds-in-the-sky-iphone-7-wallpaper-1080x1920_66d50aee72ada8437c16462cc18fe703_raw")
@@ -22,9 +23,10 @@ class ViewController: UIViewController {
         return view
     }()
     
+    // MARK: - Header
     lazy var dayDescriptoinLabel: UILabel = {
         let label = UILabel()
-        label.text = "Rainy morning"
+        label.text = "Sunny morning"
         label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
@@ -38,6 +40,7 @@ class ViewController: UIViewController {
         return label
     }()
     
+    // MARK: - Weather card with city name
     lazy var cityNameLabel: UILabel = {
         let label = UILabel()
         label.text = "Samara"
@@ -53,13 +56,13 @@ class ViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-    
+   
+    // MARK: - Weather topstack
     lazy var weatherTopStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [temperatureLabel, weatherImageView])
         stackView.axis = .horizontal
         stackView.distribution = .equalSpacing
         stackView.alignment = .center
-        stackView.spacing = 30.0
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
@@ -67,33 +70,82 @@ class ViewController: UIViewController {
     lazy var temperatureLabel: UILabel = {
         let label = UILabel()
         label.text = "22°C"
-        label.font = UIFont.systemFont(ofSize: 34, weight: .bold)
-        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
         return label
     }()
     
     lazy var weatherImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.image = #imageLiteral(resourceName: "AppIcon40x40")
-        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = #imageLiteral(resourceName: "sun")
+        imageView.contentMode = .scaleAspectFit
+        view.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
-    fileprivate func weatherViewLayout() {
-        NSLayoutConstraint.activate([
-                                        weatherView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                                        weatherView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-                                        weatherView.heightAnchor.constraint(equalToConstant: 220),
-                                        weatherView.widthAnchor.constraint(equalToConstant: 350)])
-    }
+    // MARK: - Weather bottomstack
+    lazy var weatherBottomStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [humidityStackView, windStackView])
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
     
-    fileprivate func weatherTopStackViewLayout() {
-        NSLayoutConstraint.activate([
-                                        weatherTopStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                                        weatherTopStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-                                        weatherTopStackView.heightAnchor.constraint(equalToConstant: 50),
-                                        weatherTopStackView.widthAnchor.constraint(equalToConstant: 300)])
-    }
+    lazy var humidityStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [descriptionHumidityLabel, humidityLabel])
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    lazy var descriptionHumidityLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Humidity"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var humidityLabel: UILabel = {
+        let label = UILabel()
+        label.text = "80%"
+        label.font = UIFont.systemFont(ofSize: 18, weight: .light)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var windStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [descriptionWindLabel, windLabel])
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    lazy var descriptionWindLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Wind"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var windLabel: UILabel = {
+        let label = UILabel()
+        label.text = "10 km"
+        label.font = UIFont.systemFont(ofSize: 18, weight: .light)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var dayWeatherScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        return scrollView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -121,9 +173,15 @@ class ViewController: UIViewController {
         weatherViewLayout()
         
         weatherView.contentView.addSubview(weatherTopStackView)
+        weatherImageViewLayout()
         weatherTopStackViewLayout()
+        
+        weatherView.contentView.addSubview(weatherBottomStackView)
+        humidityAndWindStackLayout()
+        weatherBottomStackViewLayout()
     }
     
+    // MARK: - Background constraints
     fileprivate func setupBackgroundLayout() {
         NSLayoutConstraint.activate([
                                         backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -141,27 +199,65 @@ class ViewController: UIViewController {
                                         containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
     }
     
+    // MARK: - Header constraints
     fileprivate func setupDescriptionLabelLayout() {
         NSLayoutConstraint.activate([
                                         dayDescriptoinLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-                                        dayDescriptoinLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60)])
+                                        dayDescriptoinLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30)])
     }
     
     fileprivate func setupWeekDayAndTimeLabelLayout() {
         NSLayoutConstraint.activate([
                                         weekDayAndTimeLabel.topAnchor.constraint(equalTo: dayDescriptoinLabel.topAnchor, constant: 30),
-                                        weekDayAndTimeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60)])
+                                        weekDayAndTimeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30)])
     }
     
+    // MARK: - Weather card with city name constraints
     fileprivate func setupCityNameLabelLayout() {
         NSLayoutConstraint.activate([
-                                        cityNameLabel.topAnchor.constraint(equalTo: weekDayAndTimeLabel.topAnchor, constant: 120),
+                                        cityNameLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 220),
                                         cityNameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)])
     }
-}
-extension UIView
-{
     
+    fileprivate func weatherViewLayout() {
+        NSLayoutConstraint.activate([
+                                        weatherView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                                        weatherView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                                        weatherView.heightAnchor.constraint(equalToConstant: 220),
+                                        weatherView.widthAnchor.constraint(equalToConstant: 350)])
+    }
+   
+    fileprivate func weatherImageViewLayout() {
+        NSLayoutConstraint.activate([
+                                        weatherImageView.heightAnchor.constraint(equalToConstant: 70),
+                                        weatherImageView.widthAnchor.constraint(equalToConstant: 70)])
+    }
+    
+    fileprivate func weatherTopStackViewLayout() {
+        NSLayoutConstraint.activate([
+                                        weatherTopStackView.topAnchor.constraint(equalTo: weatherView.topAnchor, constant: 30),
+                                        weatherTopStackView.leadingAnchor.constraint(equalTo: weatherView.leadingAnchor, constant: 30),
+                                        weatherTopStackView.heightAnchor.constraint(equalToConstant: 70),
+                                        weatherTopStackView.widthAnchor.constraint(equalToConstant: 290)])
+    }
+    
+    fileprivate func humidityAndWindStackLayout() {
+        NSLayoutConstraint.activate([
+                                        humidityStackView.heightAnchor.constraint(equalToConstant: 40),
+                                        windStackView.heightAnchor.constraint(equalToConstant: 40),
+                                        windStackView.widthAnchor.constraint(equalToConstant: 70)])
+    }
+    
+    fileprivate func weatherBottomStackViewLayout() {
+        NSLayoutConstraint.activate([
+                                        weatherBottomStackView.topAnchor.constraint(equalTo: weatherView.topAnchor, constant: 120),
+                                        weatherBottomStackView.leadingAnchor.constraint(equalTo: weatherView.leadingAnchor, constant: 30),
+                                        weatherBottomStackView.heightAnchor.constraint(equalToConstant: 70),
+                                        weatherBottomStackView.widthAnchor.constraint(equalToConstant: 290)])
+    }
+}
+extension UIVisualEffectView
+{
     func setGradientBackground(colorTop: UIColor, colorBottom: UIColor){
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [colorTop.cgColor, colorBottom.cgColor]
@@ -172,4 +268,16 @@ extension UIView
         
         self.layer.insertSublayer(gradientLayer, at: 0)
     }
+    
+    func dropShadow(color: UIColor, opacity: Float = 0.5, offSet: CGSize, radius: CGFloat = 1, scale: Bool = true) {
+        layer.masksToBounds = false
+        layer.shadowColor = color.cgColor
+        layer.shadowOpacity = opacity
+        layer.shadowOffset = offSet
+        layer.shadowRadius = radius
+
+        layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+        layer.shouldRasterize = true
+        layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+      }
 }
