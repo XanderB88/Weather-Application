1e0e9965777d5ce947e68ccfa8ba85ea08c26ccd
XanderB88 <alexxanderbykov@gmail.com>
2021-09-06 03:08:07 +0400

    Update scrollview, add all week days in scroll.

--- Weather Application/ViewController.swift
+++ Weather Application/ViewController.swift
@@ -216,6 +216,39 @@ class ViewController: UIViewController {
         return view
     }()
     
+    lazy var tuesdayLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Tue"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var tuesdayWeatherImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "sun")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var tuesdayTemperatureLabel: UILabel = {
+        let label = UILabel()
+        label.text = "20°C"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var tuesdayStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [tuesdayLabel, tuesdayWeatherImage, tuesdayTemperatureLabel])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     // MARK: - Wenesday element in scrollview
     lazy var wednesdayWeatherView: UIVisualEffectView = {
         let view = UIVisualEffectView()
@@ -224,6 +257,39 @@ class ViewController: UIViewController {
         view.effect = blurEffect
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
+    }()
+    
+    lazy var wednesdayLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Wen"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var wednesdayWeatherImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "sun")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var wednesdayTemperatureLabel: UILabel = {
+        let label = UILabel()
+        label.text = "20°C"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var wednesdayStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [wednesdayLabel, wednesdayWeatherImage, wednesdayTemperatureLabel])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
     }()
     
     // MARK: - Thursday element in scrollview
@@ -236,6 +302,39 @@ class ViewController: UIViewController {
         return view
     }()
     
+    lazy var thursdayLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Thu"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var thursdayWeatherImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "sun")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var thursdayTemperatureLabel: UILabel = {
+        let label = UILabel()
+        label.text = "20°C"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var thursdayStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [thursdayLabel, thursdayWeatherImage, thursdayTemperatureLabel])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     // MARK: - Friday element in scrollview
     lazy var fridayWeatherView: UIVisualEffectView = {
         let view = UIVisualEffectView()
@@ -244,6 +343,39 @@ class ViewController: UIViewController {
         view.effect = blurEffect
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
+    }()
+    
+    lazy var fridayLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Fri"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var fridayWeatherImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "sun")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var fridayTemperatureLabel: UILabel = {
+        let label = UILabel()
+        label.text = "20°C"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var fridayStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [fridayLabel, fridayWeatherImage, fridayTemperatureLabel])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
     }()
     
     // MARK: - Saturday element in scrollview
@@ -256,6 +388,39 @@ class ViewController: UIViewController {
         return view
     }()
     
+    lazy var saturdayLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sat"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var saturdayWeatherImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "sun")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var saturdayTemperatureLabel: UILabel = {
+        let label = UILabel()
+        label.text = "20°C"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var saturdayStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [saturdayLabel, saturdayWeatherImage, saturdayTemperatureLabel])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     // MARK: - Sunday element in scrollview
     lazy var sundayWeatherView: UIVisualEffectView = {
         let view = UIVisualEffectView()
@@ -264,6 +429,39 @@ class ViewController: UIViewController {
         view.effect = blurEffect
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
+    }()
+    
+    lazy var sundayLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sun"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var sundayWeatherImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "sun")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var sundayTemperatureLabel: UILabel = {
+        let label = UILabel()
+        label.text = "20°C"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var sundayStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [sundayLabel, sundayWeatherImage, sundayTemperatureLabel])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
     }()
     
     //    Lock portrait orientation
@@ -311,6 +509,24 @@ class ViewController: UIViewController {
         
         mondayWeatherView.contentView.addSubview(mondayStackView)
         mondayStackViewLayout()
+        
+        tuesdayWeatherView.contentView.addSubview(tuesdayStackView)
+        tuesdayStackViewLayout()
+        
+        wednesdayWeatherView.contentView.addSubview(wednesdayStackView)
+        wednesdayStackViewLayout()
+        
+        thursdayWeatherView.contentView.addSubview(thursdayStackView)
+        thursdayStackViewLayout()
+        
+        fridayWeatherView.contentView.addSubview(fridayStackView)
+        fridayStackViewLayout()
+        
+        saturdayWeatherView.contentView.addSubview(saturdayStackView)
+        saturdayStackViewLayout()
+        
+        sundayWeatherView.contentView.addSubview(sundayStackView)
+        sundayStackViewLayout()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -435,6 +651,61 @@ class ViewController: UIViewController {
                                         
                                         mondayWeatherImage.widthAnchor.constraint(equalToConstant: 45),
                                         mondayWeatherImage.heightAnchor.constraint(equalToConstant: 45)])
+    }
+    
+    fileprivate func tuesdayStackViewLayout() {
+        NSLayoutConstraint.activate([
+                                        tuesdayStackView.topAnchor.constraint(equalTo: tuesdayWeatherView.topAnchor, constant: 5),
+                                        tuesdayStackView.centerXAnchor.constraint(equalTo: tuesdayWeatherView.centerXAnchor),
+                                        
+                                        tuesdayWeatherImage.widthAnchor.constraint(equalToConstant: 45),
+                                        tuesdayWeatherImage.heightAnchor.constraint(equalToConstant: 45)])
+    }
+    
+    fileprivate func wednesdayStackViewLayout() {
+        NSLayoutConstraint.activate([
+                                        wednesdayStackView.topAnchor.constraint(equalTo: wednesdayWeatherView.topAnchor, constant: 5),
+                                        wednesdayStackView.centerXAnchor.constraint(equalTo: wednesdayWeatherView.centerXAnchor),
+                                        
+                                        wednesdayWeatherImage.widthAnchor.constraint(equalToConstant: 45),
+                                        wednesdayWeatherImage.heightAnchor.constraint(equalToConstant: 45)])
+    }
+    
+    fileprivate func thursdayStackViewLayout() {
+        NSLayoutConstraint.activate([
+                                        thursdayStackView.topAnchor.constraint(equalTo: thursdayWeatherView.topAnchor, constant: 5),
+                                        thursdayStackView.centerXAnchor.constraint(equalTo: thursdayWeatherView.centerXAnchor),
+                                        
+                                        thursdayWeatherImage.widthAnchor.constraint(equalToConstant: 45),
+                                        thursdayWeatherImage.heightAnchor.constraint(equalToConstant: 45)])
+    }
+    
+    fileprivate func fridayStackViewLayout() {
+        NSLayoutConstraint.activate([
+                                        fridayStackView.topAnchor.constraint(equalTo: fridayWeatherView.topAnchor, constant: 5),
+                                        fridayStackView.centerXAnchor.constraint(equalTo: fridayWeatherView.centerXAnchor),
+                                        
+                                        fridayWeatherImage.widthAnchor.constraint(equalToConstant: 45),
+                                        fridayWeatherImage.heightAnchor.constraint(equalToConstant: 45)])
+    }
+    
+    fileprivate func saturdayStackViewLayout() {
+        NSLayoutConstraint.activate([
+                                        saturdayStackView.topAnchor.constraint(equalTo: saturdayWeatherView.topAnchor, constant: 5),
+                                        saturdayStackView.centerXAnchor.constraint(equalTo: saturdayWeatherView.centerXAnchor),
+                                        
+                                        saturdayWeatherImage.widthAnchor.constraint(equalToConstant: 45),
+                                        saturdayWeatherImage.heightAnchor.constraint(equalToConstant: 45)])
+    }
+    
+    
+    fileprivate func sundayStackViewLayout() {
+        NSLayoutConstraint.activate([
+                                        sundayStackView.topAnchor.constraint(equalTo: sundayWeatherView.topAnchor, constant: 5),
+                                        sundayStackView.centerXAnchor.constraint(equalTo: sundayWeatherView.centerXAnchor),
+                                        
+                                        sundayWeatherImage.widthAnchor.constraint(equalToConstant: 45),
+                                        sundayWeatherImage.heightAnchor.constraint(equalToConstant: 45)])
     }
 }
 extension UIVisualEffectView {
